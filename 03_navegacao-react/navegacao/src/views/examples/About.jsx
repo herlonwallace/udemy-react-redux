8ef55f46e014d436d5bd7c6b34a0c306de92b11e
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-09-26 23:01:42 -0300

    Navegação com React: aula 1
    Introdução, Estrutura do projeto, aplicando estilo, React Router Dom v6, Navegação simples.

---
+++ 03_navegacao-react/navegacao/src/views/examples/About.jsx
@@ -0,0 +1,8 @@
+const About = props => (
+    <aside className="About">
+        <h1>Sobre</h1>
+        <p>O nosso sistema foi criado...</p>
+    </aside>
+)
+
+export default About
---
+++ 03_navegacao-react/navegacao/src/views/examples/Home.jsx
@@ -0,0 +1,7 @@
+const Home = props => (
+    <aside className="Home">
+        <h1>Início</h1>
+    </aside>
+)
+
+export default Home
